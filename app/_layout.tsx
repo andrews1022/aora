44fedc08d14f01b449863e9eccb34a64b4a12823
Andrew Shearer <andrewshearerdev@gmail.com>
2024-07-08 17:27:58 -0700

    added SplashScreen & useFonts to root layout to include custom fonts
    also quickly updated text element on home screen to use custom font to ensure it's working (it is, woot!)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,43 @@
-import { Slot, Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import type { FontSource } from "expo-font";
+
+// prevent the splash screen from auto-hiding before asset loading is complete
+SplashScreen.preventAutoHideAsync();
+
+const fonts: Record<string, FontSource> = {
+  "Poppins-Black": require("@/assets/fonts/Poppins-Black.ttf"),
+  "Poppins-Bold": require("@/assets/fonts/Poppins-Bold.ttf"),
+  "Poppins-ExtraBold": require("@/assets/fonts/Poppins-ExtraBold.ttf"),
+  "Poppins-ExtraLight": require("@/assets/fonts/Poppins-ExtraLight.ttf"),
+  "Poppins-Light": require("@/assets/fonts/Poppins-Light.ttf"),
+  "Poppins-Medium": require("@/assets/fonts/Poppins-Medium.ttf"),
+  "Poppins-Regular": require("@/assets/fonts/Poppins-Regular.ttf"),
+  "Poppins-SemiBold": require("@/assets/fonts/Poppins-SemiBold.ttf"),
+  "Poppins-Thin": require("@/assets/fonts/Poppins-Thin.ttf")
+};
 
 const RootLayout = () => {
+  const [loaded, error] = useFonts(fonts);
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
-      {/* HOME */}
-      <Stack.Screen
-        name="index"
-        options={{
-          // this hides the default header
-          headerShown: false
-        }}
-      />
-
-      {/* PROFILE */}
-      <Stack.Screen
-        name="profile"
-        options={{
-          headerShown: false
-        }}
-      />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="profile" options={{ headerShown: false }} />
     </Stack>
   );
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1
-  }
-});
--- app/index.tsx
+++ app/index.tsx
@@ -4,10 +4,10 @@ import { Text, View } from "react-native";
 const HomeScreen = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="mb-4">HomeScreen</Text>
+      <Text className="mb-4 text-4xl font-pregular">HomeScreen</Text>
 
       <Link href="/profile">
-        <Text className="bg-blue-300 text-white text-2xl">Go to Profile</Text>
+        <Text className="bg-blue-400 text-white text-2xl">Go to Profile</Text>
       </Link>
     </View>
   );
